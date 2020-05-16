3f7c18d63ab248b94169cfa995dc78868c4ae2fb
KieranLZ <2848656308@qq.com>
2020-05-16 11:37:37 +0800

    Create new_topic & entry page

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     return render(request, 'blogs/index.html')
@@ -14,5 +18,34 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request,'blogs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('blogs:topics'))
+
+    context = {"form":form}
+    return render(request, 'blogs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('blogs:topic', arg=[topic_id]))
+
+    context = {'topic':topic, 'form':form}
+    return render(request, 'blogs/new_entry.html', context)
+
 # Create your views here.
 
